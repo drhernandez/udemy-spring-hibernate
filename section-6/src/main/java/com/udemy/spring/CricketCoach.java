dad953c46a03e4f933e6aea5dc0412d667bfc6cf
Diego Hernández <drhernandez92@gmail.com>
2020-01-06 17:39:09 -0300

    section 6

---
+++ section-6/src/main/java/com/udemy/spring/CricketCoach.java
@@ -0,0 +1,39 @@
+package com.udemy.spring;
+
+public class CricketCoach implements Coach {
+
+    private String email;
+    private String team;
+
+    private FortuneService fortuneService;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Do something of cricket";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
